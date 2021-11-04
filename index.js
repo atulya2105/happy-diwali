59860949233a193038e56f15f84827e69250c40a
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2021-11-04 11:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-alert("May this Diwali fill into our lives new hopes for future and new dreams for tomorrow with lots of love wishing you very
-*Happy Diwali *✨🖤
-
-Regards
-Atulya Pandey .");
+alert("May this Diwali fill into our lives new hopes for future and new dreams for tomorrow with lots of love wishing you very. Happy Diwali  Regards Atulya Pandey.");
