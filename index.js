f045e648a4e322566b6dfa0062789ccbcb2ae77a
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2021-11-04 11:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert("May the Deepavali season bring you joy. Happy Diwali ");
+alert("May thi. Diwali fill into our lives new hopes for future and new dreams for tomorrow with lots of love wishing you very
+*Happy Diwali *✨🖤
+
+Regards
+Atulya Pandey .");
